268bf8899605f800ccf7d0c51943dca928e42f60
Ablancoc1547 <ablancoc@ucenfotec.ac.cr>
2024-10-23 01:38:08 -0400

    Creacion del servidor y el puerto a utilizar con express js

---
+++ app.js
@@ -0,0 +1,13 @@
+import express, { json } from "express";
+
+const app = express();
+app.use(json());
+app.disable("x-powered-by");
+
+//Aqui ponemos los routers que vayamos a usar
+
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
