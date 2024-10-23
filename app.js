972a6ea1b5e3493119063ead5ac75f488473618c
Ablancoc1547 <ablancoc@ucenfotec.ac.cr>
2024-10-23 01:52:48 -0400

    Creacion del router de citas listo para agregar las rutas a utilizar

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express, { json } from "express";
 import { corsMiddleware } from "./middlewares/cors";
+import { citasRouter } from "./routes/citas";
 
 const app = express();
 app.use(json());
 app.disable("x-powered-by");
 
 //Aqui ponemos los routers que vayamos a usar
+app.use("/citas", citasRouter);
 
 const PORT = process.env.PORT ?? 3000;
 
---
+++ routes/citas.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+//Aqui importamos el controlador que vamos a usar (citaController)
+
+export const citasRouter = Router();
+
+//Aqui hacemos las rutas ejemplo:
+
+// citasRouter.get("/", CitaController.getAll);
+
+// citasRouter.get("/:id", CitaController.getOne);
+
+// citasRouter.post("/", CitaController.create);
+
+// citasRouter.put("/:id", CitaController.update);
+
+// citasRouter.delete("/:id", CitaController.delete);
